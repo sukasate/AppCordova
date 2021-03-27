b9b6f46648d6cfdc7c10bd13f46da9a3f50fd525
sukasate <laras.adinara@gmail.com>
2021-03-27 20:14:50 +0700

    cordova

---
+++ www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id15161183_rianadi","7@]i9&3Ev8R9OCQj","id15161183_akademik") or die ("could not connect database");
+?>
